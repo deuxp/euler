782690d41d8b6cb7d9ea05666391e457cc83fcf6
deuxp <deuxp@tutanota.com>
2021-08-09 21:40:41 -0400

    largest-product

---
+++ project-euler/travel_pts.py
@@ -0,0 +1,10 @@
+total_pts = 273685
+first_1200_points = 250 * 1200
+remainder_points = total_pts - first_1200_points
+# add 1200 to mystery amount
+mystery_amount = (remainder_points / 200) #+ 1200
+
+print(mystery_amount)
+
+print(first_1200_points)
+print(remainder_points)
